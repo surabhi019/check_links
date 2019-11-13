8d11164da85bfa7e6bbd502342c3116ec9f14276
shan2new <shantanusinha95@gmail.com>
2019-11-14 01:06:58 +0530

    Added more urls

--- server/views.py
+++ server/views.py
@@ -12,15 +12,15 @@ from server import common_utils
 
 def index(request):
 	pageExists = True
-	# status_code_2 = urllib.request.urlopen("http://10.235.21.28:8282/CrestaL2/login/?next=/CrestaL2/").getcode()
-	# status_code_3 = urllib.request.urlopen("http://10.235.21.28:8282/CrestaL2/login/?next=/CrestaL2/").getcode()
-	# status_code_4 = urllib.request.urlopen("http://10.235.21.27:6611/CrestaL2/login/?next=/CrestaL2/").getcode()
-	# status_code_5 = urllib.request.urlopen("http://10.235.21.27:8181/CrestaL2/").getcode()
-	# status_code_6 = urllib.request.urlopen("http://10.235.21.27:2323/AITestEngine/login/?next=/AITestEngine/").getcode()
-	# status_code_7 = urllib.request.urlopen("http://10.235.21.27:8181/CrestaL2/").getcode()
-	# status_code_8 = urllib.request.urlopen("http://10.235.21.28:9292/AITestEngine/login/?next=/AITestEngine/").getcode()
-	# status_code_9 = urllib.request.urlopen("https://10.235.21.28/AITestEngine//").getcode()
-	# status_code_10 = urllib.request.urlopen("http://10.235.21.27:8181/CrestaL2/").getcode()
+	status_code_2 = urllib.request.urlopen("http://10.235.21.28:8282/CrestaL2/login/?next=/CrestaL2/").getcode()
+	status_code_3 = urllib.request.urlopen("http://10.235.21.28:8282/CrestaL2/login/?next=/CrestaL2/").getcode()
+	status_code_4 = urllib.request.urlopen("http://10.235.21.27:6611/CrestaL2/login/?next=/CrestaL2/").getcode()
+	status_code_5 = urllib.request.urlopen("http://10.235.21.27:8181/CrestaL2/").getcode()
+	status_code_6 = urllib.request.urlopen("http://10.235.21.27:2323/AITestEngine/login/?next=/AITestEngine/").getcode()
+	status_code_7 = urllib.request.urlopen("http://10.235.21.27:8181/CrestaL2/").getcode()
+	status_code_8 = urllib.request.urlopen("http://10.235.21.28:9292/AITestEngine/login/?next=/AITestEngine/").getcode()
+	status_code_9 = urllib.request.urlopen("https://10.235.21.28/AITestEngine//").getcode()
+	status_code_10 = urllib.request.urlopen("http://10.235.21.27:8181/CrestaL2/").getcode()
 
 	status_code_1=urllib.request.urlopen("http://10.235.21.28:2828/AITestEngine/login/?next=/AITestEngine/").getcode()
 
@@ -28,32 +28,32 @@ def index(request):
 	if(status_code_1 != 200):
 		pageExists = False
 
-	# if(status_code_2 != 200):
-	# 	pageExists = False 
+	if(status_code_2 != 200):
+		pageExists = False 
 
-	# if(status_code_3 != 200):
-	# 		pageExists = False
+	if(status_code_3 != 200):
+			pageExists = False
 
-	# if(status_code_4 != 200):
-	# 	pageExists = False
+	if(status_code_4 != 200):
+		pageExists = False
 
-	# if(status_code_5 != 200):
-	# 	pageExists = False
+	if(status_code_5 != 200):
+		pageExists = False
 
-	# if(status_code_6 != 200):
-	# 		pageExists = False
+	if(status_code_6 != 200):
+			pageExists = False
 
-	# if(status_code_7 != 200):
-	# 	pageExists = False 
+	if(status_code_7 != 200):
+		pageExists = False 
 
-	# if(status_code_8 != 200):
-	# 		pageExists = False
+	if(status_code_8 != 200):
+			pageExists = False
 
-	# if(status_code_9 != 200):
-	# 	pageExists = False
+	if(status_code_9 != 200):
+		pageExists = False
 
-	# if(status_code_10 != 200):
-	# 	pageExists = False 
+	if(status_code_10 != 200):
+		pageExists = False 
 
 	context = {
 		'pageExists': pageExists
